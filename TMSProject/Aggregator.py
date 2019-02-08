32e3172383ab99103b61f1a06605890e1a6646ec
pscrapy <p.nicoli@campus.unimib.it>
2019-02-08 21:33:52 +0100

    addedd aggregator for outputs

---
+++ TMSProject/Aggregator.py
@@ -0,0 +1,33 @@
+import glob
+import json
+
+reslist = glob.glob("TMSProject/redump_*_20_0.75.json")
+
+scribe= open('results_models.csv','w')
+
+scribe.write("head,quote,sign,spars,model,acc,trtime\n")
+
+for f in reslist:
+
+    with open(f,'r') as fin:
+        res = json.load(fin)
+
+    pieces = f[11:-5].split('_')
+    pieces.pop(0)
+
+    head = bool(int(pieces[0][0]))
+    quote = bool(int(pieces[0][1]))
+    sign = bool(int(pieces[0][2]))
+    #minDF = int(pieces[1])
+    #maxDF = float(pieces[2])
+
+    ndocs, nterms = res[0]['Shape']
+    spar = res[0]['Sparsity']
+    for m in res[1].keys():
+        acc = res[1]['Acc']
+        ttime = res[1]['Train_time']
+
+        scribe.write("%s,%s,%s,%s,%s,%s,%s\n" % (head, quote, sign, spar, m, acc, ttime))
+        print("%s,%s,%s,%s,%s,%s,%s\n" % (head, quote, sign, spar, m, acc, ttime))
+
+scribe.close()
